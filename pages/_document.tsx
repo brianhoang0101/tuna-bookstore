49c38fcec02e9a9409ed6d76002c4e71e614efe6
Hoàng Vũ Nam <brianhoang@Hoangs-Mac-mini.local>
2023-12-29 00:19:23 +0700

    Fix build

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -10,7 +10,7 @@ export default function Document() {
           content="TUNA Book - Tủ sách đáng quý, tunabook, tusachbinhyen, tusachgiauco, sách hay, sách kinh doanh, sách chữa lành, sách phát triển bản thân, sách phật pháp"
         />
         <meta name="google-adsense-account" content="ca-pub-4554403796307363" />
-        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4554403796307363" crossorigin="anonymous"></script>
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4554403796307363" crossOrigin="anonymous"></script>
       </Head>
       <body>
         <Main />
